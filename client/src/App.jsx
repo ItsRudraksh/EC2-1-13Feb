3cf85067bf816eff056cec562306daf3e753bf80
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2025-02-13 14:43:57 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Hello welcome to {"rudraksh's"} ec-2 instance</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import path from "path"; //FOR DEPLOYMENT
+// import url from "url"; //FOR DEPLOYMENT
+dotenv.config();
+
+const app = express();
+
+// const __filename = url.fileURLToPath(import.meta.url); //FOR DEPLOYMENT
+// const __dirname = path.dirname(__filename); //FOR DEPLOYMENT
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "../client/dist"))); //FOR DEPLOYMENT
+
+app.get("/", (req, res) => {
+  res.send("Hello World! it rudraksh ec-2 backend");
+});
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// }); //FOR DEPLOYMENT
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
